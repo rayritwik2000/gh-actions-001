89ff5842d86c4c7026f0432c3931ef55216fc7a7
rayritwik2000 <rayritwik2000@outlook.com>
2026-09-17 15:58:50 +0530

    ascii-art-withscript

---
+++ shellscript.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install cowsay -y
+ls -ltra
+cowsay -f dragon " RUN for cover, I am a DRAGON....RAWR " >> dragon.txt
+ls-ltra
+grep -i "dragon" dragon.txt
+echo "Display the file"
+cat dragon.txt
